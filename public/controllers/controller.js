c6ac470caa6bd7184db4aba74a21c25fb8e5882b
ahmetanilgur <ahmetanilgur@gmail.com>
2015-06-22 16:38:10 +0300

    ilk commit

---
+++ public/controllers/controller.js
@@ -0,0 +1,62 @@
+/// <reference path="../../typings/angularjs/angular.d.ts"/>
+var myApp = angular.module('myApp', []);
+myApp.controller('AppCtrl', ['$scope', '$http', function($scope, $http) {
+    console.log("Hello World from controller");
+
+
+var refresh = function() {
+  $http.get('/todolist').success(function(response) {
+    console.log("I got the data I requested");
+    $scope.todolist = response;
+    $scope.todo = "";
+  });
+};
+
+refresh();
+
+$scope.addTodo = function() {
+  console.log($scope.todo);
+  $http.post('/todolist', $scope.todo).success(function(response) {
+    console.log(response);
+    refresh();
+  });
+};
+
+$scope.remove = function(id) {
+  console.log(id);
+  $http.delete('/todolist/' + id).success(function(response) {
+    refresh();
+  });
+};
+
+$scope.edit = function(id) {
+  console.log(id);
+  $http.get('/todolist/' + id).success(function(response) {
+    $scope.todo = response;
+  });
+};
+
+$scope.update = function() {
+  console.log($scope.todo._id);
+  $http.put('/todolist/' + $scope.todo._id, $scope.todo).success(function(response) {
+    refresh();
+  })
+};
+$scope.update2 = function(id) {
+  console.log(id);
+  $http.put('/todolist2/' + id, $scope.todo).success(function(response) {
+    refresh();
+  })
+};
+$scope.update3 = function(id) {
+  console.log(id);
+  $http.put('/todolist3/' + id, $scope.todo).success(function(response) {
+    refresh();
+  })
+};
+
+$scope.deselect = function() {
+  $scope.todo = "";
+}
+
+}]);﻿
---
+++ server.js
@@ -0,0 +1,77 @@
+var express = require('express');
+var app = express();
+var mongojs = require('mongojs');
+var db = mongojs('todo', ['todo']);
+var bodyParser = require('body-parser');
+
+app.use(express.static(__dirname + '/public'));
+app.use(bodyParser.json());
+
+app.get('/todolist', function (req, res) {
+  console.log('I received a GET request');
+
+  db.todo.find(function (err, docs) {
+    console.log(docs);
+    res.json(docs);
+  });
+});
+
+app.post('/todolist', function (req, res) {
+  console.log(req.body.text);
+  db.todo.insert({text: req.body.text, status: 0, active: 0}, function(err, doc) {
+    res.json(doc);
+  });
+});
+
+app.delete('/todolist/:id', function (req, res) {
+  var id = req.params.id;
+  console.log(id);
+  db.todo.remove({_id: mongojs.ObjectId(id)}, function (err, doc) {
+    res.json(doc);
+  });
+});
+
+app.get('/todolist/:id', function (req, res) {
+  var id = req.params.id;
+  console.log(id);
+  db.todo.findOne({_id: mongojs.ObjectId(id)}, function (err, doc) {
+    res.json(doc);
+  });
+});
+
+app.put('/todolist/:id', function (req, res) {
+  var id = req.params.id;
+  console.log(req.body.text);
+  db.todo.findAndModify({
+    query: {_id: mongojs.ObjectId(id)},
+    update: {$inc: {status: 1}},
+    new: true}, function (err, doc) {
+      res.json(doc);
+    }
+  );
+});
+app.put('/todolist2/:id', function (req, res) {
+  var id = req.params.id;
+  console.log(req.body.text);
+  db.todo.findAndModify({
+    query: {_id: mongojs.ObjectId(id)},
+    update: {$inc: {status: 1}},
+    new: true}, function (err, doc) {
+      res.json(doc);
+    }
+  );
+});
+
+app.put('/todolist3/:id', function (req, res) {
+  var id = req.params.id;
+  console.log(req.body.text);
+  db.todo.findAndModify({
+    query: {_id: mongojs.ObjectId(id)},
+    update: {$inc: {active: 1}},
+    new: true}, function (err, doc) {
+      res.json(doc);
+    }
+  );
+});
+app.listen(3000);
+console.log("Server running on port 3000");
